05a088993db0d4fe210495289b4d94ec6cd09522
Haru Grossman <dogtaboodog@yahoo.com>
2024-01-17 15:14:37 -0600

    Adding folders

---
+++ SRC/Client/FooterBar.js
@@ -0,0 +1,9 @@
+// line separating top from navigation list 
+//     -Top disclosures
+//     -Navigation list: 
+// Shop and Learn, Apple Wallet, Account, Entertainment, Apple Store, For Business, For Education, For Healthcare, For Government, Apple Values, About Apple
+//     - Bottom: Copyright, Privacy Policy, Terms of Use, Sales and Refunds, Legal, Site Map
+     
+
+// First lets build out component that will need to be exported
+
