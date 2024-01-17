95ba1773faabb0292b4fe26f87b1fe720e41cf93
Haru Grossman <dogtaboodog@yahoo.com>
2024-01-17 16:54:06 -0600

    Adding more Drop down menu

---
+++ SRC/Client/Layout/dropDownMenuComponents/AirPodsDropDown.js
@@ -0,0 +1,28 @@
+export default function AirPodsDropDownMenu(){
+    return (
+        <>
+      <section>
+        <ul>
+            <li id="dropDownMenuHeaderText">Explore AirPods</li>
+            <li>Explore All AirPods</li>
+            <li>AirPods Pro 2nd generation</li>
+            <li>AirPods 2nd generation</li>
+            <li>AirPods 3rd generation</li>
+            <li>AirPods Max</li>
+            <li id="dropDownLittleNote">Compare AirPods</li>
+        </ul>
+        <ul>
+            <li id="dropDownMenuHeaderText">Shop AirPods</li>
+            <li>Shop AirPods</li>
+            <li>AirPods Accessories</li>
+        </ul>
+        <ul>
+            <li id="dropDownMEnuHeaderText">More from AirPods</li>
+            <li>AirPods Support</li>
+            <li>AppleCare+ for Headphones</li>
+            <li>Apple Music</li>
+        </ul>
+      </section>
+        </>
+    )
+}
---
+++ SRC/Client/Layout/dropDownMenuComponents/WatchDropDown.js
@@ -0,0 +1,36 @@
+export default function WatchDropDownMenu(){
+    return (
+        <>
+      <section>
+        <ul>
+            <li id="dropDownMenuHeaderText">Explore Watch</li>
+            <li>Explore All Apple Watch</li>
+            <li>Apple Watch Series 9</li>
+            <li>Apple Watch Ultra 2</li>
+            <li>Apple Watch SE</li>
+            <li>Apple Watch Nike</li>
+            <li>Apple Watch Hermisès</li>
+            <li id="dropDownLittleNote">Compare Watch</li>
+            <li id="dropDownLittleNote">Why Apple Watch</li>
+        </ul>
+        <ul>
+            <li id="dropDownMenuHeaderText">Shop Watch</li>
+            <li>Shop Apple Watch</li>
+            <li>Apple Watch Studio</li>
+            <li>Apple Watch Bands</li>
+            <li>Apple Watch Accessories</li>
+            <li>Apple Trade In</li>
+            <li>Financing</li>
+        </ul>
+        <ul>
+            <li id="dropDownMEnuHEaderText">More from Watch</li>
+            <li>Apple Watch Support</li>
+            <li>AppleCare+</li>
+            <li>watchOS 10</li>
+            <li>Apps by Apple</li>
+            <li>Apple Fitness+</li>
+        </ul>
+      </section>
+        </>
+    )
+}
